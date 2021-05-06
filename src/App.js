6f2dd56ea0ecb65dc10f750a0ded8b5a88c9b799
Nissim <nissimram1812@gmail.com>
2021-05-05 22:50:02 -0400

    added a title, subheader, textfield for input, and textfield for results

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+
+import Instructions from "./components/instructions/instructions"
+import Input from "./components/input/input"
+import TwitterResults from "./components/twitterResults/twitterResults"
+
+
 
 function App() {
+
+  const handleInput = ( ) => {
+    
+  }
   return (
     <div className="App">
-      <header>
-      
-      Welcome to enter the Echo Chamber 
-      
-      </header>
+      <Grid container spacing = {10} direction = "column">
+     
+          <Grid item>
+          <header>
+            <Typography variant = "h1" style = {{fontSize:"3em", marginTop: "15px"}}>  
+            Welcome to The Echo Chamber </Typography>
+          </header>
+          </Grid>
+
+          <Grid item>
+            <Instructions/>
+          </Grid>
+
+          <Grid item>
+            <Input/>
+          </Grid>
+
+          <Grid item>
+            <section>
+              <TwitterResults/>
+            </section>
+          </Grid>
+
+      </Grid>
     </div>
   );
 }
---
+++ src/components/input/input.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TextField from '@material-ui/core/TextField';
+
+const Input = ( )  => {
+
+    return(
+
+        <TextField id="standard-basic" label="Twitter Handle" />
+
+    )
+}
+
+export default Input;
