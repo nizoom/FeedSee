6012a19fa93055efcaf3c007bf10117a9469e9d5
Nissim <nissimram1812@gmail.com>
2021-06-09 18:51:35 -0400

    finalized graphic gif  for word cloud

--- client/src/components/wordcloud/wordcloud.js
+++ client/src/components/wordcloud/wordcloud.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
-import Goo from 'gooey-react'
+//import Goo from 'gooey-react'
+import Typography from '@material-ui/core/Typography';
 import randomColor from "randomcolor"
-import "./testcss.css"
+import "./wordcloud.css"
+import gif from "./../../media/tweetgifv6.gif"
+//import test from "./../../media/test.png"
+///Users/nissimram / Desktop / Programming / Twitter Project / twitter - project / client / src / media / tweetGifv4.gif
 
 const styles = {
     large: {
@@ -25,9 +29,11 @@ const MyCloud = (props) => {
     //     // }, 5000);
     // }
 
+
+
     const droplets = props.text.map((droplet) => (
         <div key={props.text.indexOf(droplet)} className="droplet"
-            style={{ fontSize: `${droplet.frequency * .6}em`, color: randomColor(), fontWeight: "bold", }}
+            style={{ fontSize: `${droplet.frequency * .6}em`, color: randomColor(), fontWeight: "bold", padding: "2px" }}
         >  {droplet.word} </div>
     ))
 
@@ -35,9 +41,22 @@ const MyCloud = (props) => {
 
     return (
         <div>
-            <h1> Word Cloud </h1>
+
+
+            <Typography variant="h2" style={{ color: "#14394B", padding: "10px" }}> Word Cloud </Typography>
             <div className='app-outer'>
-                {droplets}
+
+                <div style={{
+                }}>
+
+                    <img src={gif} alt="tweetGif" style={{ borderRadius: "16px", height: "50%" }} />
+
+                </div>
+
+                <div className="cloud">
+                    {droplets}
+                </div>
+
 
 
             </div>
@@ -51,8 +70,21 @@ export default MyCloud;
 
 //{droplets}
 
+// <img src={gif} alt="tweetGif" />
+
+//<img src={gif} alt="tweetGif">  </img>
+
+//border: "solid", borderColor: "black", borderWidth: ".5px",
+
 
 {/* 
+
+
+    width: "450px", height: "600px",
+                    overflow: "hidden"
+
+
+
                           <Goo instenity="weak" style={{ animation: 'left 4s linear infinite' }}>
                     <svg width="192" height="192">
 
